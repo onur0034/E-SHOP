2593a0c9acf0f75c4fed90b158e3c866c656e3b9
onur <onur.un0034@gmail.com>
2022-11-01 19:51:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">E-Shop!</h2>
+      </div>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon"/>
+      </div>
+
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest!</span>
+          <span className="nav__itemLineTwo">Sign In!</span>
+        </div>
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <div className="nav__item">
+          <LocalGroceryStoreIcon fontSize="large" />
+          <span className="nav__itemLineTwo">0</span>
+        </div>
+      </div>
     </div>
   );
 }
